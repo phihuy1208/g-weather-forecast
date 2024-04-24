5f7abe813a409dbd26107afba5ff333878541ff8
ntphuy2001 <ntphuy2001@gmail.com>
2024-04-25 04:24:08 +0700

    Delete file welcome laravel

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\HistoryController;
-use App\Http\Controllers\LocationController;
 use App\Http\Controllers\WeatherController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
